73e51d77876e7624b5f101f8c4978ed7961d55ce
Francis <Francis@192.168.1.10>
2022-02-14 14:09:42 -0500

    added possibility to use I and L for glVertexAttrib*Pointer

--- gl/+glmu/ArrayPointer.m
+++ gl/+glmu/ArrayPointer.m
@@ -14,22 +14,32 @@ classdef ArrayPointer < glmu.Array
             obj.Edit(varargin{:});
         end
 
-        function Edit(obj,buffer,normalized)
+        function Edit(obj,buffer,normalized,attrib)
             % buffer = glmu.Buffer | buffer data
             % optional normalized = normalize flags for each, default false
             if nargin < 2, return, end
             if nargin < 3, normalized = false; end
+            if nargin < 4, attrib = ''; end
+            if ~iscell(attrib), attrib = {attrib}; end
             if ~isa(buffer,'glmu.Buffer')
                 buffer = glmu.Buffer(obj.gl.GL_ARRAY_BUFFER,buffer);
             end
             nb = numel(buffer.id);
+            if nb > 1 && isscalar(attrib), attrib = repmat(attrib,nb,1); end
             obj.norm = obj.Const(normalized,nb);
             obj.Bind;
             for i=1:nb
                 buffer.Bind(i,obj.gl.GL_ARRAY_BUFFER);
                 sz = buffer.sz(i,1);
                 type = buffer.type(i);
-                obj.gl.glVertexAttribPointer(i-1,sz,type,obj.norm(i),0,0);
+                args = {i-1, sz, type};
+                if isempty(attrib{i})
+                    obj.gl.glVertexAttribPointer(args{:},obj.norm(i),0,0);
+                else
+                    f = str2func(['glVertexAttrib' attrib{i} 'Pointer']);
+                    f(obj.gl,args{:},0,0);
+                end
+                
                 obj.gl.glEnableVertexAttribArray(i-1)
             end
             obj.n = min(buffer.sz(:,2));
